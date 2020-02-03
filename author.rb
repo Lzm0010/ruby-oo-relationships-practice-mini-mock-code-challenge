5773686680dd7d9098c4939ca8ca47fe13d37174
github username <github email address>
2020-02-03 10:40:21 -0500

    Done

--- author.rb
+++ author.rb
@@ -1,1 +1,40 @@
+class Author
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select{|book| book.author == self}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, word_count, self)
+    end
+
+    def total_words
+        self.books.reduce(0){|total, book| total + book.word_count}
+    end
+
+    def self.most_words
+        words = 0
+        verbose_author = nil
+        self.all.each{|author|
+            if author.total_words > words
+                words = author.total_words
+                verbose_author = author
+            end
+        }
+        verbose_author
+    end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,13 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+ernie = Author.new("Ernest Hemmingway")
+alex = Author.new("Alexander Dumas")
+ernie.write_book("Old man and the sea", 3000)
+ernie.write_book("For whom the bell tolls", 4000)
+alex.write_book("3 Musketeers", 5000)
+alex.write_book("The count of Monte Cristo", 35000)
+
 binding.pry
 
 
